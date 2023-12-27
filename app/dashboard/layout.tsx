808e52f4f4915b732bc6bb11319c98de43712228
Willerson Alves <willerson@fuerzastudio.com.br>
2023-12-26 22:59:18 -0300

    capitulo 8 e 9

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import Loading from './(overview)/loading';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -6,7 +7,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        {children !== null && children !== undefined ? children : <Loading />}
+      </div>
     </div>
   );
 }
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,12 +2,11 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+  
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -33,8 +32,7 @@ export default async function LatestInvoices({
                     src={invoice.image_url}
                     className="mr-4 rounded-full"
                     width={32}
-                    height={32}
-                  />
+                    height={32} alt={''}                  />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
                       {invoice.name}
